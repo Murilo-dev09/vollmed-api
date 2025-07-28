3c858098203df57ded5c2b5c0c2507bd9a20ea22
Murilo-dev09 <murilomorales09@gmail.com>
2025-07-28 19:08:36 -0300

    primeiro commit do projeto Spring Boot

---
+++ src/main/java/med/vol/api/medico/Medico.java
@@ -0,0 +1,67 @@
+package med.vol.api.medico;
+
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.vol.api.endereco.Endereco;
+
+@Table(name = "medicos")
+@Entity(name = "Medicos")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+    private boolean ativo;
+
+    /*Obs importante: aqui estamos fazendo a conecao com o front end atraves do DTO
+    Ou seja quando dizemos this.nome = dados.nome(); o this.nome e o nome dessa classe
+    que vai receber o dados.nome(variavel do DTO(poderia ser outro nome) ou seja esse nome do dados.nome
+    se refere a classe do DTO e atribui o valor que foi passado no front para o objeto Medico do atributo nome.
+     */
+
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.crm = dados.crm();
+        this.endereco = new Endereco(dados.endereco());
+        this.especialidade = dados.especialidade();
+        this.ativo = true;
+    }
+
+    public void atualizarInformacoes(@Valid DadosAtualizacaoMedico dados) {
+        if(dados.nome() != null){
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null){
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+
+    }
+
+    public void excluir() {
+        this.ativo = false;
+    }
+}
